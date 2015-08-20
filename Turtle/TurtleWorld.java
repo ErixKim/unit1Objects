e4b2e85fe7083794110265c47b1e12a62cac5c48
ErixKim <edkim@NN123-HA-18.HS.sd203.org>
2015-08-20 14:12:02 -0500

    IAN JOYCE
    A TURTLE THAT CREATES IAN JOYCE

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,26 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(180);
+        turtle.forward(25);
+        turtle.turn(90);
+        turtle.forward(150);
+        turtle.turn(45);
+        turtle.forward(80);
+        turtle.turn(180);
+        turtle.forward(80);
+       
+        
+        
+        
     }
 }
