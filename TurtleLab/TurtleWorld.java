eafbdb7644a7bddf17158dd09efba57df24d4aa6
ErixKim <edkim@stu.naperville203.org>
2015-09-03 14:02:32 -0500

    Turtle Lab
    asdf

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -4,16 +4,25 @@ public class TurtleWorld
 {
     public static void main(String args[])
     {
-        World turtleWorld = new World();
+        World turtleWorld = new World(1000,1000);
         Turtle turtle = new Turtle(turtleWorld);
         Turtle turtle2 = new Turtle(turtleWorld);
         turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle2.setPenColor(Color.RED);
         turtle2.penDown();
-        turtle.forward(355);
-        turtle2.turn(180);
-        turtle2.forward(355);
+        
+        int x = 10;
+        int y = 15;
+        for (int i=1; i < 9999; i++){
+            turtle.forward(x);
+            turtle.turn(90);
+            x += 10;
+            turtle2.forward(y);
+            
+    }
+       
+        
     }
 }
         
